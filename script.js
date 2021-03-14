8bb730912ef4badebc5b498a4b07e4bc8183102f
Ali Shooshtari <shooshtariali05@gmail.com>
2021-03-14 13:54:51 +0330

    accounts data

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/////////////////////////////////////////////////
+// Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
+const account1 = {
+    owner: "Ali shooshtari",
+    movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+
+    movementsDates: [
+        "2020-11-18T21:31:17.178Z",
+        "2020-12-23T07:42:02.383Z",
+        "2021-01-28T09:15:04.904Z",
+        "2021-04-01T10:17:24.185Z",
+        "2021-05-08T14:11:59.604Z",
+        "2021-03-07T17:01:17.194Z",
+        "2021-03-12T23:36:17.929Z",
+        "2021-03-13T10:51:36.790Z",
+    ],
+    currency: "EUR",
+    locale: "de-DE", //
+};
+
+const account2 = {
+    owner: "Pedram Zangeneh",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+
+    movementsDates: [
+        "2020-11-01T13:15:33.035Z",
+        "2020-11-30T09:48:16.867Z",
+        "2020-12-25T06:04:23.907Z",
+        "2021-01-25T14:18:46.235Z",
+        "2021-02-05T16:33:06.386Z",
+        "2021-04-10T14:43:26.374Z",
+        "2021-06-25T18:49:59.371Z",
+        "2021-07-26T12:01:20.894Z",
+    ],
+    currency: "USD",
+    locale: "en-US",
+};
+
+const accounts = [account1, account2];
